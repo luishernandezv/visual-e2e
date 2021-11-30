9dd73fbcee972a0f0ff98c331af80ed566465af3
Luis Velasquez <luis.velasquez@saucelabs.com>
2021-11-29 20:35:30 -0800

    Adding webdriverio tests

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # visual-e2e
-Simple Visual E2E Test
+
+Simple Visual E2E Test Repo
+
+## Prerequisite
+
+- Node installed
+- WebdriverIO installed (Will be installed in next step)
+- Have a test framework installed e.g Mocha, Jest (Will be installed in next step)
+
+### Install dependencies
+
+#### Linux/macOS
+
+```bash
+npm install
+```
+
+#### Windows PowerShell
+
+```powershell
+npm install
+```
+
+### Define environment variables
+
+Replace values in <> for actual values e.g `export SAUCE_USERNAME="developerAccount"`
+
+#### Linux/macOS
+
+```bash
+export SAUCE_USERNAME="<REPLACE THIS WITH A VALID SAUCE USERNAME>"
+export SAUCE_ACCESS_KEY="<REPLACE THIS WITH A VALID SAUCE ACCESS KEY>"
+export SCREENER_API_KEY="<REPLACE THIS WITH A VALID SCREENER API KEY>"
+```
+
+#### Windows powershell
+
+```powershell
+$Env:SAUCE_USERNAME = "<REPLACE THIS WITH A VALID SAUCE USERNAME>"
+$Env:SAUCE_ACCESS_KEY = "<REPLACE THIS WITH A VALID SAUCE ACCESS KEY>"
+$Env:SAUCE_ACCESS_KEY = "<REPLACE THIS WITH A VALID SCREENER API KEY>"
+```
+
+## Run tests
+
+NOTE: Your **first test run will fail** as there are no baselines to compare against to. More info [here](https://docs.saucelabs.com/visual/e2e-testing/setup/#5-run) or configure your test to not fail on new states using `failOnNewStates` capability, more info [here](https://docs.saucelabs.com/visual/e2e-testing/commands-options/#saucevisual-capability-options)
+
+### Using only webdriverio
+
+```bash
+npm run webdriverio
+```
+
+### Using webdriverio and mocha
+
+```bash
+npm run mocha:test
+```
+
+### Using webdriverio and jest
+
+```bash
+npm run jest:test
+```
---
+++ simple-test.js
@@ -0,0 +1,62 @@
+const { remote } = require('webdriverio');
+
+//Define the configuration for the webdriver test
+const config = {
+  hostname: 'hub.screener.io',
+  port: 443,
+  protocol: 'https',
+  path: '/wd/hub',
+  capabilities: {
+    browserName: 'chrome',
+    browserVersion: '87.0',
+    'sauce:options': {
+      username: process.env.SAUCE_USERNAME,
+      accessKey: process.env.SAUCE_ACCESS_KEY,
+    },
+    'sauce:visual': {
+      apiKey: process.env.SCREENER_API_KEY,
+      projectName: 'sauce-demos/swag-labs',
+      viewportSize: '1280x1024',
+    },
+  },
+};
+
+let browser;
+
+async function simpleTest() {
+  browser = await remote(config);
+
+  //Navigate to the test site
+  await browser.url('http://saucedemo.com');
+
+  //Initialize the visual test
+  await browser.execute('/*@visual.init*/', 'Swag Labs');
+
+  await browser.execute('/*@visual.snapshot*/', 'Login');
+
+  //Log in
+  const usernameInput = await browser.$('#user-name');
+  const passwordInput = await browser.$('#password');
+  const loginBtn = await browser.$('#login-button');
+  await usernameInput.setValue('standard_user');
+  await passwordInput.setValue('secret_sauce');
+  await loginBtn.click();
+  const inventoryElm = await browser.$('#inventory_container');
+  await inventoryElm.waitForDisplayed();
+  await browser.execute('/*@visual.snapshot*/', 'Products');
+
+  //End visual test and get result
+  const result = await browser.execute('/*@visual.end*/');
+
+  if (!result.passed) {
+    throw new Error(result.message);
+  }
+
+  await browser.deleteSession();
+
+  return 'Test Successful';
+}
+
+simpleTest()
+  .then((result) => console.log(result))
+  .catch((e) => console.error(e));
